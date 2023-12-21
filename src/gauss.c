aba05bf546827f9bd6537ce2113201a582825443
Mateusz Czarnecki <Matjasunio>
2023-12-21 19:38:51 +0100

    Eliminacja Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,25 @@
 #include "gauss.h"
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+int eliminate(Matrix *mat, Matrix *b) {
+	int n = mat->r;
+		 double **a = mat->data;
+		 double *l = b->data;
 
-		return 0;
+		  for (int k = 0; k < n - 1; k++) {
+			   	 for (int w = k + 1; w < n; w++) {
+					  if (a[k][k] == 0.0) {
+						  	   return 1;
+							    }
+					      double wsp = a[w][k] / a[k][k];
+
+					         for (int i = k; i < n; i++) {
+							               a[w][i] -= a[k][i] * wsp;
+								          }
+						   l[w] -= l[k] * wsp;
+						          	 }
+				 					    }
+
+		  			    return 0;
 }
+
 
