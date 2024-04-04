480a33a57c2441bdd993370e509850b11a9d96df
Matjasunio <153275335+Matjasunio@users.noreply.github.com>
2024-04-04 21:17:19 +0200

    Update odczyt.c

--- odczyt.c
+++ odczyt.c
@@ -4,59 +4,51 @@
 int odczyt(FILE *f, FILE *out) {
     char* buffer = malloc(40);
 
-    if (buffer == NULL) {
-        return -1;
-    }
-
     int liczba_slow;
 
-    if (fread(buffer, 40, 1, f) != 1) {
-        free(buffer);
-        return -1;
-    }
+    fread(buffer, 40, 1, f);
 
     char* pointer = buffer;
     pointer += 29;
+
     liczba_slow = *(int*)pointer;
 
     unsigned char wartosc;
     char slowo;
 
     free(buffer);
+
     buffer = malloc(liczba_slow * 3);
 
-    if (buffer == NULL) {
-        return -1;
-    }
-
-    if (fread(buffer, liczba_slow * 3, 1, f) != 1) {
-        free(buffer);
-        return -1;
-    }
+    fread(buffer, liczba_slow * 3, 1, f);
 
     pointer = buffer;
 
-
     int counter = 0;
 
-    for (int i = 0; i < liczba_slow; i++) {
+    FILE* out = fopen("nowy", "w");
+
+    for(int i = 0; i < liczba_slow; i++)
+    {
         pointer++;
         slowo = *pointer;
         pointer++;
         wartosc = *pointer;
         pointer++;
 
-        for (int y = 0; y < wartosc+1; y++) {
+        for(int y = 0; y < wartosc+1; y++)
+        {
             fprintf(out, "%c", slowo);
             counter++;
-            if (counter == 513) {
+            if(counter == 513)
+            {
                 fprintf(out, "\n");
                 counter = 0;
             }
         }
     }
 
-    fclose(out);
-    free(buffer);
+
     return 0;
+
 }
