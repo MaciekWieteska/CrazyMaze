5b65eaca3a1284cf266423ba1c1a5320d6b1cffe
Matjasunio <153275335+Matjasunio@users.noreply.github.com>
2024-04-04 15:08:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,12 +11,17 @@ int main(int argc, char*argv[])
 		fprintf(stderr,"Blad, nie moge czytac z: %s, %s", argv[0],argv[1]);
 		return -13;
 	}
+FILE *out = argc > 2 ? fopen(argv[2], "w"): stdout;
+if(in == NULL){
+fprintf(out, "Nie moge pisac do: %s, %s", argv[0], argv[2]);
+}
 	kol = kolumny(in);
 	rewind(in);
 	wier = wiersze(in);
 	rewind(in);
 	dzielenie(in, wier, kol);
 	rewind(in);
+odczyt(in, out);
 	fclose(in);
 	return 0;
 }
