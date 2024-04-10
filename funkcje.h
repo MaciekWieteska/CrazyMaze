4414145ebfd9564e4e0de46e4a5b8c6e843c3190
Matjasunio <153275335+Matjasunio@users.noreply.github.com>
2024-04-10 12:33:31 +0200

    Update funkcje.h

--- funkcje.h
+++ funkcje.h
@@ -8,6 +8,6 @@ int kolumny(FILE *in);
 void dzielenie(FILE *in, int wiersze, int kolumny);
 char* nazwa(int w, int k);
 int odczyt(FILE *in, FILE *out);
-void murowanie(char labirynt[][100], int wiersze, int kolumny)
+void murowanie(FILE *in, FILE *out,int wiersze,int kolumny);
 #endif
 
