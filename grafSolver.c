2eff2082840170ee6f32f7ca7763e7c9405c01a2
Matjasunio <153275335+Matjasunio@users.noreply.github.com>
2024-04-25 22:34:41 +0200

    Update grafSolver.c

--- grafSolver.c
+++ grafSolver.c
@@ -4,66 +4,39 @@
 #include <ctype.h>
 
 int grafSolver(FILE *in, FILE *out) {
-    int najmniejsza_suma = 400;
-    int znaleziono_k = 0;
-
-    char *linia = malloc(sizeof(char) * 500); 
-
-    while (fgets(linia, 100, in) != NULL) {
-        int dlugosc_linii = strlen(linia);
-        if (linia[dlugosc_linii - 1] != '\n') { 
-            int nowy_rozmiar = 2 * dlugosc_linii + 1;
-            linia = realloc(linia, sizeof(char) * nowy_rozmiar); 
-            if (linia == NULL) {
-                fprintf(stderr, "Błąd alokacji pamięci.\n");
-                return -1;
-            }
-        }
-
-        if (strchr(linia, 'k') != NULL) {
-            int suma = 0;
-            for (int i = 0; linia[i] != '\0'; i++) {
-                if (isdigit(linia[i])) {
-                    suma += abs(linia[i] - '0');
-                }
-            }
-            if (suma < najmniejsza_suma) {
-                najmniejsza_suma = suma;
-                znaleziono_k = 1;
-            }
-        }
-    }
-
-    free(linia); 
-
-    if (znaleziono_k == 0) {
-        fprintf(out, "Brak linii zawierających literę 'k'.\n");
-        return -1;
-    }
-
-    fseek(in, 0, SEEK_SET);
-    char sciezka[najmniejsza_suma];
+    char sciezka[30000]; 
     fgets(sciezka, sizeof(sciezka), in);
-fprintf(out, "START\n");
+
     int dlugosc = strlen(sciezka);
-    for (int i = 0; i < dlugosc; i += 2) {
-        int liczba = abs(sciezka[i] - '0');
-        char kierunek = sciezka[i + 1];
-        switch (kierunek) {
-            case 'U':
-                fprintf(out, "FORWARD %d\n", liczba);
-                break;
-            case 'L':
-                fprintf(out, "LEFT %d\n", liczba);
-                break;
-            case 'R':
-                fprintf(out, "RIGHT %d\n", liczba);
-                break;
-            case 'D':
-                fprintf(out, "DOWN %d\n", liczba);
-                break;
+    int i = dlugosc - 1;
+    while (i >= 0) {
+        if (isdigit(sciezka[i])) {
+            int j = i - 1;
+            while (j >= 0 && isdigit(sciezka[j])) {
+                j--;
+            }
+            int liczba_krokow = atoi(sciezka + j + 1);
+            char kierunek = sciezka[j];
+            switch (kierunek) {
+                case 'U':
+                    fprintf(out, "UPPER %d\n", liczba_krokow);
+                    break;
+                case 'L':
+                    fprintf(out, "LEFT %d\n", liczba_krokow);
+                    break;
+                case 'R':
+                    fprintf(out, "RIGHT %d\n", liczba_krokow);
+                    break;
+                case 'D':
+                    fprintf(out, "DOWN %d\n", liczba_krokow);
+                    break;
+            }
+            i = j - 1;
+        } else {
+            i--;
         }
     }
-fprintf(out,"STOP\n"); 
-    return najmniejsza_suma;
+
+    fprintf(out, "STOP\n"); 
+    return 0;
 }
